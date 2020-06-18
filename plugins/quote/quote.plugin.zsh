a80943bc836777f0e0d432c7d81995c32dabd9e2
Joonmo Yang <dev@remagpie.com>
2020-06-18 14:12:30 +0900

    Use local variable in the quote plugin

--- plugins/quote/quote.plugin.zsh
+++ plugins/quote/quote.plugin.zsh
@@ -1,7 +1,7 @@
 if [[ -o interactive ]]; then
-	QUOTE=$(shuf -n1 $(dirname $0:a)/quotes.txt)
-	AUTHOR=$(echo $QUOTE | cut -d'|' -f1 | sed -e 's/^[[:space:]]*//' -e 's/*[[:space:]]//')
-	MESSAGE=$(echo $QUOTE | cut -d'|' -f2 | sed -e 's/^[[:space:]]*//' -e 's/*[[:space:]]//')
+	local QUOTE=$(shuf -n1 $(dirname $0:a)/quotes.txt)
+	local AUTHOR=$(echo $QUOTE | cut -d'|' -f1 | sed -e 's/^[[:space:]]*//' -e 's/*[[:space:]]//')
+	local MESSAGE=$(echo $QUOTE | cut -d'|' -f2 | sed -e 's/^[[:space:]]*//' -e 's/*[[:space:]]//')
 
 	echo $MESSAGE
 	echo " - $AUTHOR"
