3f0394af0ed14782a7f9ca39635c80de0d5487ec
Joonmo Yang <dev@remagpie.com>
2020-06-18 15:02:07 +0900

    Use gshuf for macos in quote plugin

--- plugins/quote/quote.plugin.zsh
+++ plugins/quote/quote.plugin.zsh
@@ -1,7 +1,16 @@
 if [[ -o interactive ]]; then
-	local QUOTE=$(shuf -n1 $(dirname $0:a)/quotes.txt)
-	local AUTHOR=$(echo $QUOTE | cut -d'|' -f1 | sed -e 's/^[[:space:]]*//' -e 's/*[[:space:]]//')
-	local MESSAGE=$(echo $QUOTE | cut -d'|' -f2 | sed -e 's/^[[:space:]]*//' -e 's/*[[:space:]]//')
+	local QUOTEDB="$(dirname $0:a)/quotes.txt"
+	local ROW
+	case $OSTYPE in
+		linux*)
+			ROW=$(shuf -n1 $QUOTEDB)
+			;;
+		darwin*)
+			ROW=$(gshuf -n1 $QUOTEDB)
+			;;
+	esac
+	local AUTHOR=$(echo $ROW | cut -d'|' -f1 | sed -e 's/^[[:space:]]*//' -e 's/*[[:space:]]//')
+	local MESSAGE=$(echo $ROW | cut -d'|' -f2 | sed -e 's/^[[:space:]]*//' -e 's/*[[:space:]]//')
 
 	echo $MESSAGE
 	echo " - $AUTHOR"
