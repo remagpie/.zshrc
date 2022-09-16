5fb305a4aacd4cf4b03e1f05f18214fa2088279e
Joonmo Yang <dev@remagpie.com>
2022-09-16 15:53:08 +0900

    Add git alias

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -1,3 +1,9 @@
-export GIT_PAGER='less -R -X -F'
-
 alias g="git"
+
+export GIT_PAGER='less -R -X -F'
+if ! grep -zq '# Automatically added by zsh git plugin\s\s*\[include\]' $HOME/.gitconfig; then
+	echo '' >> $HOME/.gitconfig
+	echo '# Automatically added by zsh git plugin' >> $HOME/.gitconfig
+	echo '[include]' >> $HOME/.gitconfig
+	echo "\tpath = ${0:A:h}/gitconfig" >> $HOME/.gitconfig
+fi
